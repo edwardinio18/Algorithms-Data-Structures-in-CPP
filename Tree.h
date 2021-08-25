3b59f90ee85bd30e2e155de806e310fc960dd9b2
Edward Iakab <edwardiakab@yahoo.com>
2021-08-25 17:31:41 +0300

    UPDATED CODE

---
+++ Tree.h
@@ -0,0 +1,9 @@
+template <typename t_Data>
+class Tree {
+public:
+	virtual void traversal() const = 0;
+	virtual void insert(const t_Data &data) = 0;
+	virtual void remove(const t_Data &data) = 0;
+	virtual const t_Data &getMaxValue() const = 0;
+	virtual const t_Data &getMinValue() const = 0;
+};
